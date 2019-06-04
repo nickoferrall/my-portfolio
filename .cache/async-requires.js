29b36b2144a3287ebc5adbdd34b6c0858032863e
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-06-04 12:32:20 -0500

    Added cache

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -1,9 +1,22 @@
 // prefer default export if available
-const preferDefault = m => m && m.default || m
+const preferDefault = m => (m && m.default) || m;
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("/Users/nick/Documents/Lambda/Portfolio/my-portfolio/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */)
-}
+  "component---cache-dev-404-page-js": () =>
+    import(
+      "/Users/nick/Documents/Lambda/Portfolio/my-portfolio/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */
+    ),
+  "component---src-pages-404-js": () =>
+    import(
+      "/Users/nick/Documents/Lambda/Portfolio/my-portfolio/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */
+    ),
+  "component---src-pages-index-js": () =>
+    import(
+      "/Users/nick/Documents/Lambda/Portfolio/my-portfolio/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */
+    )
+};
 
-exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "/Users/nick/Documents/Lambda/Portfolio/my-portfolio/.cache/data.json")
-
+exports.data = () =>
+  import(
+    /* webpackChunkName: "pages-manifest" */ "/Users/nick/Documents/Lambda/Portfolio/my-portfolio/.cache/data.json"
+  );
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -1,10 +1,22 @@
-const { hot } = require("react-hot-loader/root")
+const { hot } = require("react-hot-loader/root");
 
 // prefer default export if available
-const preferDefault = m => m && m.default || m
-
+const preferDefault = m => (m && m.default) || m;
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/nick/Documents/Lambda/Portfolio/my-portfolio/.cache/dev-404-page.js")))
-}
-
+  "component---cache-dev-404-page-js": hot(
+    preferDefault(
+      require("/Users/nick/Documents/Lambda/Portfolio/my-portfolio/.cache/dev-404-page.js")
+    )
+  ),
+  "component---src-pages-404-js": hot(
+    preferDefault(
+      require("/Users/nick/Documents/Lambda/Portfolio/my-portfolio/src/pages/404.js")
+    )
+  ),
+  "component---src-pages-index-js": hot(
+    preferDefault(
+      require("/Users/nick/Documents/Lambda/Portfolio/my-portfolio/src/pages/index.js")
+    )
+  )
+};
